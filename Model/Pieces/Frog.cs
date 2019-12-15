a2c472499b29a1540ed8b24be1fa05088e02e13c
zaqmju1 <Jesse.Henderson@maine.edu>
2019-12-15 15:48:20 -0500

    Started frog and SmartPlayer

---
+++ Model/Pieces/Frog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Pieces
+{
+    public class Frog : BasePiece
+    {
+        private Vector[] directions = { new Vector(1, 1),
+            new Vector(-1, 1)
+        };
+
+        protected override char Char => '☊';
+
+        public override BasePiece[][,] GetMoves(BasePiece[,] board)
+        {
+            int jumpCount = 0;
+            var[] jumpLoc = new var[4];
+            var boards = new List<BasePiece[,]>();
+            jumpLoc[jumpCount] = Location;
+
+            Action<Vector> baseDirections = d =>
+            {
+                jumpCount++;
+                var looking = jumpLoc[jumpCount] + d;
+                bool captured = board[looking.X, looking.Y] != null;
+                if (IsOnBoard(looking) && board[looking.X, looking.Y]?.Color != Color)
+                {
+                    // Moving without jumping
+                    if (!captured)
+                    {
+                        boards.Add(CloneBoardAndMove<Frog>(board, looking));
+                    }
+
+                    // Recursion for jumping multiple times
+                    else if (captured && IsOnBoard(jumpLoc[jumpCount] + d) && board[looking.X, looking.Y]?.Color != Color)
+                    {
+                        looking += d;
+                        boards.Add(CloneBoardAndMove<Frog>(board, looking));
+                        foreach (Vector d in directions)
+                        {
+                            jumpLoc[jumpCount] = Looking;
+                            baseDirections(d);
+                        }
+                        jumpCount--;
+                    }
+                }
+            };
+
+            foreach(Vector d in directions)
+            {
+                baseDirections(jumpLoc[jumpCount]);
+            }
+
+			return boards.ToArray(); 
+        }
+    }
+}
---
+++ Model/Players/SmartPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Players
+{
+    public class SmartPlayer : BasePlayer
+    {
+        private static readonly Random R = new Random();
+
+        public string Name;
+        public Color Color;
+
+        virtual public int SmartMove(BasePiece[][,] options)
+        {
+            int bestIndex = 0;
+            double bestScore = ScoreMove(options[0]);
+
+            for (int i = 1; i < options.Length; i++)
+            {
+                double score = ScoreMove(options[i]);
+                if (score > bestScore)
+                {
+                    bestIndex = i;
+                    bestScore = score;
+                }
+            }
+
+            return bestIndex;
+        }
+
+        virtual protected double SmartScore(BasePiece[,] options)
+        {
+            return R.NextDouble() - .5;
+        }
+    }
+}
